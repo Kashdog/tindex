b0ee340c8354bd1da21da043a8875002fe3104cc
Kashdog <aneeshkashalikar@gmail.com>
2021-02-05 11:51:11 -0500

    Python

--- app.py
+++ app.py
@@ -10,10 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-capp = celery.Celery('tindex')
+#capp = celery.Celery('tindex')
 
 
-@capp.task
 def scrape(name):
     subprocess.run(["twint" , "-u", name, "-o", name, "--json"])
     
